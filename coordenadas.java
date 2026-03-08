15f357d451e50ef5eeb5cb370fcbe0ab56c25ce9
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-03-08 21:09:08 +0100

    Add Mision_Programadora class with main method

---
+++ coordenadas.java
@@ -0,0 +1,10 @@
+public class Mision_Programadora {
+    public static void main(String[] args) {
+        double latitud = 40.4167;
+        int zona = (int) latitud;
+
+        System.out.println(latitud);
+        System.out.println(zona);
+
+        }
+}
