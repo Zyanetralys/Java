abba4fd374f974e3327af9c4126b708320918d0d
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-03-08 22:20:02 +0100

    Add Vigilancia_Patron class with scanning loop

---
+++ Vigilancia_Patron..java
@@ -0,0 +1,9 @@
+public class Vigilancia_Patron {
+    public static void main(String[] args) {
+        // Bucle de vigilancia 5 ciclos de escaneo
+        for (int ciclo = 1; ciclo <= 5; ciclo++) {
+            System.out.println("PULSO DETECTADO - CICLO" + ciclo);
+            }
+            System.out.println("ANALISIS COMPLETO -PATRON EXTRAIDO");
+    }
+}
