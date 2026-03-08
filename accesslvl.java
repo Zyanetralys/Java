be2ff5ca5cdaa50037e76bc628c8eb52b667ddff
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-03-08 17:56:02 +0100

    Add Mision_Programadora class with main method

---
+++ accesslvl.java
@@ -0,0 +1,9 @@
+public class Mision_Programadora {
+    public static void main(String[] args) {
+        String codename = "Recluta_01";
+        int nivel_acceso = 1;
+        
+        System.out.println(codename);
+        System.out.println(nivel_acceso);
+    }
+}
