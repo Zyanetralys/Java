d444d4aa2cb1cc81f904a663dfdc53e9c931b8ad
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-03-08 18:09:15 +0100

    Add Mision_Programadora class with security key calculation

---
+++ operadores_aritmeticos.java
@@ -0,0 +1,9 @@
+public class Mision_Programadora {
+    public static void main (String[] args) {
+        int nivel_acceso = 1;
+        int multiplicador = 100;
+        int clave_seguridad = nivel_acceso * multiplicador;
+
+        System.out.println(clave_seguridad);
+    }
+}
