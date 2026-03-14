07990e9e1d4b99a12b63d86edf9b541d25e69a5a
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-03-14 21:18:07 +0100

    Create if_else_supervivencia.java

---
+++ if_else_supervivencia.java
@@ -0,0 +1,22 @@
+public class Ordenes4_Supervivencia {
+public static void main(String[] args) {
+
+double bateria = 34.5 / 2.1;
+double contacto = 400.0 / 15.0
+
+if (bateria < 10) {
+System.out.println("Apagar sistemas no esenciales");
+}
+else {
+System.out.println("Mantener sistemas activos");
+}
+
+if (contacto <= 50) {
+System.out.println("Lanzar granada");
+}
+else {
+System.out.println("Mantener cobertura");
+}
+
+}
+}
