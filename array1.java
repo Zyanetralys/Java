63aa11fae54a14238f6814b74f9c38528e2ea44f
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-03-15 01:24:05 +0100

    Update objetivos array with new agent names

---
+++ array1.java
@@ -0,0 +1,10 @@
+public class ListaPurga {
+public static void main(String[] args) {
+
+String[] objetivos = {"Agente01", "Agente02", "Agente03", "Agente04", "Agente05"};
+
+for (int i = 0; i < objetivos.length; i++) {
+System.out.println("Objetivo" + objetivos[i] + ": Localizada. Esperando orden.");
+}
+}
+}
