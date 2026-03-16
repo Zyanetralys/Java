78434f0981f668e53dbb5d1d003955af33bc8dfd
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-03-16 16:48:05 +0100

    Create salida1.java

---
+++ salida1.java
@@ -0,0 +1,19 @@
+Public class BreakTest {
+
+Public static void Main(String[] arg) {
+
+     for (int i = 1; i <= 100; i++) {
+
+        if (i == 5) {
+
+          break;
+
+        }
+
+         System.out.print(i);
+
+     }
+
+ }
+
+}
