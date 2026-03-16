88e2f9c7a62627821c5b72f2fa488c6a71959b20
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-03-16 19:04:28 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+package codigo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduzca el nombre: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Introduzca los apellidos: ");
+        String apellidos = scanner.nextLine();
+
+        System.out.print("Introduzca su raza (Humano, Elfo, Orco): ");
+        String raza = scanner.nextLine();
+
+        Heroe heroe = new Heroe(nombre, apellidos, raza);
+
+        heroe.mostrarInfo();
+
+        scanner.close();
+    }
+}
