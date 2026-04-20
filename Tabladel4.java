52638068a70d09fb44de0884cee3dcb8095fc461
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-20 23:46:35 +0200

    Create Tabladel4.java

---
+++ Tabladel4.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Tabla de Multiplicar del 4");
+        System.out.println("--------------------------");
+        System.out.println("4x1= "+ 4);
+        System.out.println("4x2= "+ 8);
+        // sigue
+    }
+}
