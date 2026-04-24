bd064c7645dad17e84a491f7b8796d1c21653d55
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-24 17:59:05 +0200

    Add number classification logic in mayormenor.java
    Implement a loop to classify input numbers as positive or negative.

---
+++ DAM/mayormenor.java
@@ -0,0 +1,19 @@
+int num;
+
+do
+
+{
+
+System.out.println("Introduce un número:");
+
+pedirEntradaNumero;
+
+if (num > 0)
+
+System.out.println("POSITIVO");
+
+else if (num < 0)
+
+System.out.println("NEGATIVO");
+
+} while (num != 0);
