b68fcafa02127235fcd4d033724332f2c0df916d
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-24 16:50:05 +0200

    Create Variables.java

---
+++ DAM/Variables.java
@@ -0,0 +1,31 @@
+package core;
+
+public class EntryPoint {
+
+    public static void main(String[] args) {
+        //Declaración e inicialización de variables
+        //Enteros cortos, enteros y enteros largos.
+        //Sintaxis: tipo nombreVariable;
+        //Rango de enteros es de -2^31, -2^31
+        //Tipo short son 16bits = -32768, 32767
+        //Tipo long son 64bits = -2^63, 2^63
+        //Tipo float con 7 decimales.
+        //Tipo double con 16 decimales.
+
+        int a=1,b=2,c=0;
+        long l=32767;
+        char caracter='h';
+        float f=1.56667f;
+        double dd = 1.39;
+
+        System.out.println("a:" + a);
+        System.out.println("b:" + b);
+        System.out.println("c:" + c);
+        System.out.println("caracter:" + caracter);
+        System.out.println("float:" + f);
+        System.out.println("double:" + dd);
+
+
+    }
+
+}
