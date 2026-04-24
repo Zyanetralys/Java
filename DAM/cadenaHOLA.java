d48938f97c5ababae59a57984445a3901895bbcc
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-24 17:35:44 +0200

    Add cadenaHOLA.java with a simple Java program

---
+++ DAM/cadenaHOLA.java
@@ -0,0 +1,31 @@
+¿Es correcto el siguiente código?
+
+
+
+public class Program {
+
+
+
+public static void main (String[] arg) {
+
+
+
+/*comentario externo
+
+//comentario interno
+
+mas comentario externo
+
+*/
+
+System.out.println("HOLA");
+
+
+
+
+
+}
+
+
+
+}
