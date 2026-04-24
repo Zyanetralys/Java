2576341612ca8a0a35d84a6a289e2fb90ea2a707
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-24 18:08:26 +0200

    Create Main class to demonstrate array access
    Add a Main class with a main method that prints an element from an array.

---
+++ DAM/arrayorden.java
@@ -0,0 +1,17 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    // Si tenemos el siguiente array:
+
+    int[] array = {1, 0, 0, 1, 1, 1, 0, 0};
+
+
+
+    // ¿Qué nos mostrará esta salida en pantalla? Respuesta: 1 (cuenta en la enumeración 0, 1, 2, 3)
+
+    System.out.println(array[3]);
+
+  }
+
+}
