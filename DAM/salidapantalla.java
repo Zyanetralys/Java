62ec2456060691e99fc7e1d9473a411184cbf7e9
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-24 17:27:47 +0200

    Create salidapantalla.java

---
+++ DAM/salidapantalla.java
@@ -0,0 +1,17 @@
+public class ConsoleApp2 {
+
+
+
+public static void main (String[] arg){
+
+
+
+System.out.println("Bienvenidos a mi primer programa en Java");
+
+System.out.println("Mi nombre es MJ");
+
+
+
+}
+
+}
