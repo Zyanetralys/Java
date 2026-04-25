37cb253352ef5002e37f6c20822f01a2d88c622b
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-25 20:41:40 +0200

    Update contadoroperativas.java

--- contadoroperativas.java
+++ contadoroperativas.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Agentes activos: " + Agente.obtenerContador()); // 0
         
-        Agente valeria = new Agente("one");
-        Agente kaela = new Agente("two");
-        Agente rina = new Agente("three");
+        Agente one = new Agente("one");
+        Agente two = new Agente("two");
+        Agente three = new Agente("three");
         
         System.out.println("Agentes activos: " + Agente.obtenerContador()); // 3
         System.out.println("Agente: " + one.getNombre()); // one
