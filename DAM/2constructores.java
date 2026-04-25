1cdd8eab7db429205a04676618b833bde998f592
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-25 11:06:58 +0200

    Create 2constructores.java

---
+++ DAM/2constructores.java
@@ -0,0 +1,27 @@
+public class Jugadores {
+
+  //Atributos
+
+  private String nombre;
+
+  private String liga;
+
+public Jugadores() {
+
+  this.nombre = "Equipo";
+
+  this.liga= "1";
+
+}
+
+public Jugadores (String nombre, String liga) {
+
+  this.nombre = nombre;
+
+  this.liga = liga
+
+}
+
+ //Métodos
+
+}
