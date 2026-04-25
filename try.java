04562f9e8fa565a4f7fdef030242ae69c8dcb576
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-25 18:15:39 +0200

    Create try.java

---
+++ try.java
@@ -0,0 +1,11 @@
+try {
+    // Código que puede lanzar una excepción
+} 
+
+catch (TipoDeExcepcion e) {
+    // Código para manejar la excepción
+} 
+
+finally {
+    // Código que se ejecuta siempre, ocurra o no una excepción
+}
