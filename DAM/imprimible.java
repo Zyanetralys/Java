bb49f594be752d3a5bccef67dd239914d832d4f8
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-25 23:30:51 +0200

    Create imprimible.java

---
+++ DAM/imprimible.java
@@ -0,0 +1,5 @@
+interface Imprimible {
+    void imprimir();
+}
+class Documento implements Imprimible { ... } // Implementación específica
+class Imagen implements Imprimible { ... }    // Implementación distinta
