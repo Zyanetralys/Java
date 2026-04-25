2ccb57d38651cd46175cf1590a022579d92cda7f
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-25 11:08:26 +0200

    Add instanciar.java with class instantiation examples
    Added example code for class instantiation in Java.

---
+++ DAM/instanciar.java
@@ -0,0 +1,17 @@
+//Declaración
+
+nombre_clase nombre_variable;
+
+//Creación
+
+nombre_variable = new nombre_clase ();
+
+//Declaración creación
+
+nombre_clase nombre_variable = new nombre_clase ();
+
+//Declaración creación
+
+nombre_clase nombre_variable = new nombre_clase ();
+
+  
