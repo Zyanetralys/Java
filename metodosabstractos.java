0ec74efe0e5e2dad1b506cb341af8489fcce3208
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-25 23:37:15 +0200

    Add interface Figura and Circulo class implementation

---
+++ metodosabstractos.java
@@ -0,0 +1,6 @@
+interface Figura { double calcularArea(); }
+class Circulo implements Figura {
+    private double radio;
+    public Circulo(double radio) { this.radio = radio; }
+    @Override public double calcularArea() { return Math.PI * radio * radio; }
+}
