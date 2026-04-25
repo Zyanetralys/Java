d97f4d5f165c3896e0d74156560ecccd19792b4a
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-25 11:06:18 +0200

    Create clsaeProfesor2.java

---
+++ DAM/clsaeProfesor2.java
@@ -0,0 +1,29 @@
+public class Profesores {
+
+  //Atributos
+
+  private String nombre;
+
+  private String curso;
+
+  //Constructor vacio
+
+  public Profesores() {
+
+   this.nombre = "Colegio";
+
+   this.curso = "Historia";
+
+  }
+
+  //Constructor con parámetros
+
+  public Profesores (String nombre, String curso) {
+
+   this.nombre = nombre;
+
+   this.curso = curso;
+
+  }
+
+}
