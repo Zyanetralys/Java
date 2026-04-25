a00f3805351db2f97d3619557ca1121460d677fc
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-25 10:59:48 +0200

    Create claseProfesores.java

---
+++ claseProfesores.java
@@ -0,0 +1,14 @@
+ublic class Profesores {
+ //Atributos
+  private String nombre;
+  private String clase
+ //Constructor vacío
+public Profesores() {
+  this.nombre = "POO"
+    this.clase = "3B";
+}
+ //Constructor con parámetros
+ public Profesores(String nombre, String clase) {
+  this.nombre = nombre;
+  this.clase = clase
+} }
