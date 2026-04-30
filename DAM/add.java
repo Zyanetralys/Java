af2f62563bf580294e40e695e1f1f4dd2b50ba83
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-30 22:32:34 +0200

    Create add.java

---
+++ DAM/add.java
@@ -0,0 +1,12 @@
+// Opción correcta:
+letras.add(2, 'C');
+
+// Antes: ["A", "B", "D", "E"]
+//                 ↑
+//           posición 2
+
+// Después de add(2, 'C'):
+// ["A", "B", "C", "D", "E"]
+//                 ↑
+//           'C' insertada en posición 2
+//           'D' y 'E' se desplazan a la derecha
