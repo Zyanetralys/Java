d2b06a877d0447d3f0025a8cd65d47e308cdd16b
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-05-01 04:26:08 +0200

    Create resto.java

---
+++ DAM/resto.java
@@ -0,0 +1,11 @@
+int resultado = 10 % 3;
+// resultado = 1
+// Porque 10 ÷ 3 = 3 (cociente) con resto 1
+
+// Ejemplo práctico: ¿Es par o impar?
+int numero = 7;
+if (numero % 2 == 0) {
+    System.out.println("PAR");
+} else {
+    System.out.println("IMPAR");  // Se imprime esto
+}
