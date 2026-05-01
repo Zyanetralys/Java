626b632407778454ec8c07f442ef59b1b5c831b8
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-05-01 04:29:32 +0200

    Create votar.java

---
+++ DAM/votar.java
@@ -0,0 +1,8 @@
+// Ambas condiciones deben ser TRUE
+int edad = 25;
+boolean tieneDNI = true;
+
+if (edad >= 18 && tieneDNI) {
+    System.out.println("Puede votar");
+}
+// Solo entra si edad >= 18 Y tieneDNI es true
