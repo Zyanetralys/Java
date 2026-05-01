a5720d35c000d57c87ca135ae6c637a1c83fc431
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-05-01 04:25:26 +0200

    Add pre-increment and post-increment examples
    Add examples of pre-increment and post-increment in Java.

---
+++ DAM/preincremento.java
@@ -0,0 +1,10 @@
+int x = 5;
+x++;  // x ahora es 6
+      // Aumenta UNA unidad
+
+// Dos formas:
+int a = 5;
+int b = a++;  // b=5, a=6 (post-incremento)
+
+int c = 5;
+int d = ++c;  // d=6, c=5 (pre-incremento)
