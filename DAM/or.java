867652ea7d973ed7d17e280e354c6bf6e5e61afb
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-05-01 04:30:44 +0200

    Create or.java

---
+++ DAM/or.java
@@ -0,0 +1,7 @@
+// Al menos UNA condición debe ser TRUE
+int nota = 7;
+boolean examenExtra = false;
+
+if (nota >= 5 || examenExtra) {
+    System.out.println("APROBADO");  // Entra porque nota >= 5
+}
