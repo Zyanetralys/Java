562f151de7448af5e77019c546397489ff81fcb2
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-05-03 01:25:54 +0200

    Create threat.java

---
+++ threat.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String codename = "Valkiria";
+        int threatLevel = 4;
+        double latExtraccion = 40.4098;
+        boolean enlaceActivo = true;
+        
+        System.out.println(codename + " | Alerta: " + threatLevel + " | Lat: " + latExtraccion + " | Enlace: " + enlaceActivo);
+    }
+}
